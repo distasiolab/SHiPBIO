20b1a29a536b598793f57be28d35b6339bf36526
Marcello DiStasio <ChellyD65@users.noreply.github.com>
2024-05-17 13:12:08 -0400

    Makefile fun

--- Makefile
+++ Makefile
@@ -1,16 +1,44 @@
+
+## Path to base directory for the project; should contain directories 'src', 'data', and 'calc'
+##
+##
+BASEDIR := "/Users/mmd47/Google Drive/My Drive/DiStasio Lab/DiStasio Lab Share/02 Analysis/Muscle_IBM/SHiPBIO"
+##
+
 SOURCE = ./src/
 CALC = ./calc/
 
-all : $(CALC)retinas_all_unfiltered_snRNAseq_imputed_GIMVI.h5ad
 
-$(CALC)retinas_all.h5ad :
-	python $(SOURCE)Preprocess.py
+PREPROCESS_RESULT := $(CALC)IBM_muscle_all.h5ad
+INTEGRATE_RESULT := integrate_result.txt
+CLUSTER_RESULT := cluster_result.txt
 
-$(CALC)retinas_all_integrated_harmony_unfiltered.h5ad : $(CALC)retinas_all.h5ad
-	python $(SOURCE)IntegrateHarmony.py
 
-$(CALC)retinas_all_unfiltered_snRNAseq_imputed_GIMVI.h5ad : $(CALC)retinas_all_integrated_harmony_unfiltered.h5ad
-	python $(SOURCE)Cluster_CellCharter.py
+.dummy: preprocess integrate cluster
 
-$(CALC)retinas_all_unfiltered_snRNAseq_imputed_GIMVI_ClustersLabeled_scDRS_GWAS_scores.h5ad' : $(CALC)retinas_all_unfiltered_snRNAseq_imputed_GIMVI_ClustersLabeled.h5ad
-	python $(SOURCE)GWAS_Analysis.py
+all: preprocess
+
+preprocess: $(PREPROCESS_RESULT)
+	@echo "Preprocessing completed."
+	@echo $(PREPROCESS_RESULT) "exists."
+
+integrate: $(INTEGRATE_RESULT)
+	@echo "Integration completed."
+
+cluster: $(CLUSTER_RESULT)
+	@echo "Clustering completed."
+
+
+$(PREPROCESS_RESULT):
+	mkdir -p $(CALC)
+	@echo "Preprocessing..."
+	python $(SOURCE)Preprocess.py -b $(BASEDIR)
+
+$(INTEGRATE_RESULT):
+	@echo "Batch Integration..."
+	#python $(SOURCE)IntegrateHarmony.py -b $(BASEDIR)
+
+$(CLUSTER_RESULT):
+	@echo "Clustering..."
+	#python $(SOURCE)Cluster_CellCharter.py -b $(BASEDIR)
+
--- src/Preprocess.py
+++ src/Preprocess.py
@@ -10,22 +10,22 @@ import anndata as ad
 import squidpy as sq
 import scanpy as sc
 
-import os
+import sys, os
 
 parser = argparse.ArgumentParser()
-parser.add_argment('--basepath', type=str)
+parser.add_argument('-b', '--basepath', type=str, help='Path to base directory for the project; should contain directories \'data\' and \'calc\'')
+args = parser.parse_args()
 
-
-#FILEPATHBASE = '/Users/mmd47/Library/CloudStorage/GoogleDrive-mmd47@yale.edu/My Drive/DiStasio Lab/DiStasio Lab Share/'
-FILEPATHBASE = '/home/mdistasio/YaleGoogleDrive/DiStasio Lab/DiStasio Lab Share/'
+FILEPATHBASE = args.basepath
 
 SAVEFIGS = True
 if SAVEFIGS:
-    IMGDIR = os.path.join(FILEPATHBASE,'02 Analysis/annData_ManualAnnotate/img/')
+    IMGDIR = os.path.join(FILEPATHBASE,'img')
 
 
 # --------------------------------------------------------------------------------
-# Prior to this loading, each *_anndata.h5ad should be annotated with
+# If manual spatial annotations are desired, then Prior to this loading, each
+# *_anndata.h5ad file should be annotated with
 #
 #   1. MakeFullSizeClustersImage.py
 #   2. QuPath (https://qupath.github.io/), to draw ROIs and export with Export_Annotations_GeoJSON.groovy
@@ -35,162 +35,35 @@ if SAVEFIGS:
 
 print('Loading data files and selecting annotated regions...')
 
-## MACULAR RETINA
+## Muscle data
 
-# File 1    
-filename = os.path.join(FILEPATHBASE,'03 Data','Retina_SlideSeq_Curio','A22_3781_AMD_SlideSeq_001','A0052_029_anndata_annotated.h5ad')
-adata = ad.read_h5ad(filename)
+datadir = os.path.join(FILEPATHBASE, 'data')
+files = os.listdir(datadir)
+h5ad_files = [file for file in files if file.endswith('.h5ad')]
 
-library_id = 'A22_3781'
-adata.uns["spatial"] = dict()
-adata.uns["spatial"][library_id] = dict()
-retina1 = adata[(adata.obs['Retina_1'] == True) | (adata.obs['Retina_2'] == True)]
-
-# File 2
-filename = os.path.join(FILEPATHBASE,'03 Data/Retina_SlideSeq_Curio/A23-914_OS_Ctrl_SlideSeq_001/A0052_030_anndata_annotated.h5ad')
-adata = ad.read_h5ad(filename)
-
-library_id = 'A23-914'
-adata.uns["spatial"] = dict()
-adata.uns["spatial"][library_id] = dict()
-retina2 = adata[adata.obs['Retina']]
-
-# File 3
-filename = os.path.join(FILEPATHBASE,'03 Data/Retina_SlideSeq_Curio/A23-1277_AMD_SlideSeq_001/OUTPUT/A23-1277-OS_macula/A23-1277-OS_macula_anndata_annotated.h5ad')
-adata = ad.read_h5ad(filename)
-
-library_id = 'A23-1277'
-adata.uns["spatial"] = dict()
-adata.uns["spatial"][library_id] = dict()
-retina3 = adata[adata.obs['Retina']]
-
-# File 4
-filename = os.path.join(FILEPATHBASE,'03 Data/Retina_SlideSeq_Curio/A23-1279-OS_SlideSeq_001/23-1279-OS_OUTPUT/OUTPUT/A23-1279-OS_macula/A23-1279-OS_macula_anndata_annotated.h5ad')
-adata = ad.read_h5ad(filename)
-
-library_id = 'A23-1279'
-adata.uns["spatial"] = dict()
-adata.uns["spatial"][library_id] = dict()
-retina4 = adata[adata.obs['Retina']]
-
-# File 5
-filename = os.path.join(FILEPATHBASE,'03 Data/Retina_SlideSeq_Curio/A23-1422_Ctrl_Macula_SlideSeq_001/OUTPUT/A23-1422_-_macula/A23-1422_-_macula_anndata_annotated.h5ad')
-adata = ad.read_h5ad(filename)
-
-library_id = 'A23-1422'
-adata.uns["spatial"] = dict()
-adata.uns["spatial"][library_id] = dict()
-retina5 = adata[adata.obs['Retina']]
-
-# File 6
-filename = os.path.join(FILEPATHBASE,'03 Data/Retina_SlideSeq_Curio/A23-1425_Ctrl_Macula_SlideSeq_001/OUTPUT/A23-1425_-_macula1/A23-1425_-_macula1_anndata_annotated.h5ad')
-adata = ad.read_h5ad(filename)
-
-library_id = 'A23-1425-M1'
-adata.uns["spatial"] = dict()
-adata.uns["spatial"][library_id] = dict()
-retina6 = adata[adata.obs['Retina']]
-
-
-# File 7
-filename = os.path.join(FILEPATHBASE,'03 Data/Retina_SlideSeq_Curio/A23-1152_OD_AMD_SlideSeq_002/OUTPUT/A23-1152_-_Mac_AMD/A23-1152_-_Mac_AMD_anndata_annotated.h5ad')
-adata = ad.read_h5ad(filename)
-
-library_id = 'A23-1152_OD'
-adata.uns["spatial"] = dict()
-adata.uns["spatial"][library_id] = dict()
-retina7 = adata[adata.obs['Retina']]
-
-
-## PERIPHERAL RETINA
-
-# File 8
-filename = os.path.join(FILEPATHBASE,'03 Data/Retina_SlideSeq_Curio/Peripheral_Retina/1279_Peripheral_SlideSeq_001/OUTPUT/1279_P/1279_P_anndata_annotated.h5ad')
-adata = ad.read_h5ad(filename)
-
-library_id = 'A23-1279_P'
-adata.uns["spatial"] = dict()
-adata.uns["spatial"][library_id] = dict()
-retina8 = adata[adata.obs['Retina']]
-
-
-# File 9
-filename = os.path.join(FILEPATHBASE,'03 Data/Retina_SlideSeq_Curio/Peripheral_Retina/1341-1_Peripheral_SlideSeq_001/OUTPUT/1341-1_P/1341-1_P_anndata_annotated.h5ad')
-adata = ad.read_h5ad(filename)
-
-library_id = 'A23-1341_P'
-adata.uns["spatial"] = dict()
-adata.uns["spatial"][library_id] = dict()
-retina9 = adata[adata.obs['Retina']]
-
-
-# File 10
-filename = os.path.join(FILEPATHBASE,'03 Data/Retina_SlideSeq_Curio/Peripheral_Retina/1422_P_Peripheral_SlideSeq_001/OUTPUT/1422_P/1422_P_anndata_annotated.h5ad')
-adata = ad.read_h5ad(filename)
-
-library_id = 'A23-1422_P'
-adata.uns["spatial"] = dict()
-adata.uns["spatial"][library_id] = dict()
-retina10 = adata[adata.obs['Retina']]
-
-# File 11
-filename = os.path.join(FILEPATHBASE,'03 Data/Retina_SlideSeq_Curio/Peripheral_Retina/1425-3_Peripheral_SlideSeq_001/OUTPUT/1425-3_P/1425-3_P_anndata_annotated.h5ad')
-adata = ad.read_h5ad(filename)
-
-library_id = 'A23-1425-3_P'
-adata.uns["spatial"] = dict()
-adata.uns["spatial"][library_id] = dict()
-retina11 = adata[adata.obs['Retina']]
-
-
+sample = 1
+samples_all = dict()
+for filename in h5ad_files:
+    filepath = os.path.join(datadir, filename)
+    print("Loading data from " + filepath)
+    adata = ad.read_h5ad(filepath)
+    library_id = 'IBM_Muscle_bx'
+    adata.uns["spatial"] = dict()
+    adata.uns["spatial"][library_id] = dict()
+    samples_all["Muscle_1"+str(sample)] = adata
+    print('Loaded')
+    sample = sample + 1
+print('Done loading all data sets')
 
 ### Done Loading 
-
 
 # --------------------------------------------------------------------------------
 # Defining Sample-Level Characteristics
 # --------------------------------------------------------------------------------
 
-r_all = {"R1": retina1,
-         "R2": retina2,
-         "R3": retina3,
-         "R4": retina4,
-         "R5": retina5,
-         "R6": retina6,
-         "R7": retina7,
-         "R8": retina8,
-         "R9": retina9,
-         "R10": retina10,
-         "R11": retina11}
-
-SampleKey = {"R1": "A22_3781",
-             "R2": "A23-914",
-             "R3": "A23-1277",
-             "R4": "A23-1279",
-             "R5": "A23-1422",
-             "R6": "A23-1425-M1",
-             "R7": "A23-1152_OD",
-             "R8": "A23-1279_P",
-             "R9": "A23-1341_P",
-             "R10": "A23-1422_P",
-             "R11": "A23-1425-3_P"}
-
-AnatLoc = {"R1": "macula_retina",
-           "R2": "macula_retina",
-           "R3": "macula_retina",
-           "R4": "macula_retina",
-           "R5": "macula_retina",
-           "R6": "macula_retina",
-           "R7": "macula_retina",
-           "R8": "peripheral_retina",
-           "R9": "peripheral_retina",
-           "R10": "peripheral_retina",
-           "R11": "peripheral_retina"}
-
-
-
-
-
+SampleKey = {"Muscle_1": "DM IBM 1",
+             "Muscle_2": "PTV IBM 2"
+             }
 
 # --------------------------------------------------------------------------------
 # Concatenation of all datasets into one
@@ -198,32 +71,31 @@ AnatLoc = {"R1": "macula_retina",
 
 print('Concatenating...')
 
-
-retinas_all = ad.concat(r_all, label="dataset", uns_merge="first", join='outer')
+all_samples = ad.concat(samples_all, label="dataset", uns_merge="first", join='outer')
+all_samples.obs_names_make_unique()
 
 # Clean up the NAs in manual annotation columns in adata.obs, which should be boolean
-cs = retinas_all.obs.select_dtypes(include='object').columns
-retinas_all.obs[cs] = retinas_all.obs[cs].astype('boolean').fillna(False)
+cs = all_samples.obs.select_dtypes(include='object').columns
+all_samples.obs[cs] = all_samples.obs[cs].astype('boolean').fillna(False)
 
-retinas_all.raw = retinas_all
-retinas_all.layers["counts"] = retinas_all.X.copy()   
+all_samples.raw = all_samples
+all_samples.layers["counts"] = all_samples.X.copy()   
 
 library_id = 'Curio_Seeker_v1.1_AllRetinas'
-retinas_all.uns["spatial"] = dict()
-retinas_all.uns["spatial"][library_id] = dict()
+all_samples.uns["spatial"] = dict()
+all_samples.uns["spatial"][library_id] = dict()
 
 
 ## Sample-level info
-retinas_all.uns["SampleKey"] = SampleKey
-retinas_all.uns["Anatomic_Location"] = AnatLoc
+all_samples.uns["SampleKey"] = SampleKey
 
 
 
 # --------------------------------------------------------------------------------
 # Save concatenated data
 # --------------------------------------------------------------------------------
-out_filename = os.path.join(FILEPATHBASE,'02 Analysis', 'annData_ManualAnnotate', 'calc', 'retinas_all.h5ad')
-retinas_all.write(out_filename)
+out_filename = os.path.join(FILEPATHBASE, 'calc', 'IBM_muscle_all.h5ad')
+all_samples.write(out_filename)
 
 print('Saved concatenated data to: ' + out_filename)
 print('Done!')
