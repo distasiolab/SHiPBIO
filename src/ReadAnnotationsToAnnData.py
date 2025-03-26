dd34aa71f416cf65eff3aa20ddd2335056ac395b
Marcello DiStasio <ChellyD65@users.noreply.github.com>
2025-03-25 22:11:26 -0400

    Fixed by Abi

--- src/ReadAnnotationsToAnnData.py
+++ src/ReadAnnotationsToAnnData.py
@@ -1,15 +1,8 @@
 import argparse
 import os
-import xml.etree.ElementTree as ET
 import geojson
-
-from numba import jit
-
-
 import numpy as np
-import pandas as pd
 import anndata as ad
-import scanpy as sc
 
 ## --------------------------------------------------------------------------------
 ## Input arguments
@@ -39,35 +32,25 @@ print("Output AnnData file: {0}".format(outfilename))
 ## Method Definitions
 ## --------------------------------------------------------------------------------
 
-def dim(a):
-    if not type(a) == list:
-        return []
-    return [len(a)] + dim(a[0])
-
-
-@jit(nopython=True)
 def PointsInPolygon(points, poly):
-    x,y = points[:,0], points[:,1]
+    x, y = points[:,0], points[:,1]
     n = len(poly)
-    inside = np.zeros(len(x),np.bool_)
-    p2x = 0.0
-    p2y = 0.0
-    p1x,p1y = poly[0]
-    for i in range(n+1):
-        p2x,p2y = poly[i % n]
-        idx = np.nonzero((y > min(p1y,p2y)) & (y <= max(p1y,p2y)) & (x <= max(p1x,p2x)))[0]
-        if len(idx):    # <-- Fixed here. If idx is null skip comparisons below.
+    inside = np.zeros(len(x), np.bool_)
+    p2x, p2y = 0.0, 0.0
+    p1x, p1y = poly[0]
+    for i in range(n + 1):
+        p2x, p2y = poly[i % n]
+        idx = np.nonzero((y > min(p1y, p2y)) & (y <= max(p1y, p2y)) & (x <= max(p1x, p2x)))[0]
+        if len(idx):  # If idx is non-empty, proceed with the calculation
             if p1y != p2y:
-                xints = (y[idx]-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                xints = (y[idx] - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
             if p1x == p2x:
                 inside[idx] = ~inside[idx]
             else:
                 idxx = idx[x[idx] <= xints]
                 inside[idxx] = ~inside[idxx]    
-
-        p1x,p1y = p2x,p2y
-    return inside 
-
+        p1x, p1y = p2x, p2y
+    return inside
 
 ## --------------------------------------------------------------------------------
 ## Main Method
@@ -76,7 +59,6 @@ def PointsInPolygon(points, poly):
 with open(args.coords) as f:
     gj = geojson.load(f)
 FeatureCollection = gj['features']
-
 
 Annotations = []
 
@@ -87,13 +69,12 @@ for F in FeatureCollection:
         Annotations.append(F)
     except KeyError:
         pass
-    
-AnnotationNames = list(set([A["properties"]["classification"]["name"] for A in Annotations]))
 
+AnnotationNames = list(set([A["properties"]["classification"]["name"] for A in Annotations]))
 
 print("\n")
 print("Found {0} annotations.".format(len(Annotations)))
-print("Found {0} UNIQUE annotations: ".format(len(AnnotationNames))+" ".join(AnnotationNames))
+print("Found {0} UNIQUE annotations: ".format(len(AnnotationNames)) + " ".join(AnnotationNames))
 
 ## --------------------------------------------------------------------------------
 ## Read in AnnData and assign annotations to observations based on
@@ -106,26 +87,28 @@ adata = ad.read_h5ad(args.file)
 for AN in AnnotationNames:
     adata.obs[AN] = False
 
-X_origin = [min(adata.obsm['X_spatial'][:,0]),min(adata.obsm['X_spatial'][:,1])]
+X_origin = [min(adata.obsm['X_spatial'][:, 0]), min(adata.obsm['X_spatial'][:, 1])]
 
 print("\n")
 for Annotation in Annotations:
-    if len(dim(Annotation['geometry']['coordinates'][0])) > 2:
-        for coord_set in Annotation['geometry']['coordinates']:
-            for c in coord_set:
-                InPolygon = PointsInPolygon(np.array(adata.obsm['X_spatial'] - X_origin), np.array(c))
-                adata.obs[Annotation["properties"]["classification"]["name"]][np.where(InPolygon)[0]] = True
-    else:
-        coords = np.array(Annotation['geometry']['coordinates'])
+    
+    if Annotation['geometry']['type'] == 'Polygon':
+        # Single polygon, just use the coordinates directly
+        coords = np.array(Annotation['geometry']['coordinates'][0]) 
         InPolygon = PointsInPolygon(np.array(adata.obsm['X_spatial'] - X_origin), coords)
         adata.obs[Annotation["properties"]["classification"]["name"]][np.where(InPolygon)[0]] = True
+    elif Annotation['geometry']['type'] == 'MultiPolygon':
+        # Multiple polygons MultiPolygon, iterate over each
+        for poly in Annotation['geometry']['coordinates']:
+            coords = np.array(poly[0])  
+            InPolygon = PointsInPolygon(np.array(adata.obsm['X_spatial'] - X_origin), coords)
+            adata.obs[Annotation["properties"]["classification"]["name"]][np.where(InPolygon)[0]] = True
 
     print("{0} contains {1} points.".format(Annotation["properties"]["classification"]["name"], adata.obs[Annotation["properties"]["classification"]["name"]].sum()))
-
 
 adata.write(outfilename)
 print("\n")
 print("Wrote AnnData file: {0}".format(outfilename))
-    
+
 print("\n\n\nDone!")
 
