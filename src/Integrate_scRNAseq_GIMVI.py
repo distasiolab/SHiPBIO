fedaec2bc656cb23ead141b9cdf3b71f4165da8d
Marcello DiStasio <marcello.distasio@yale.edu>
2025-07-08 08:57:21 -0400

    GIMVI fixes

--- src/Integrate_scRNAseq_GIMVI.py
+++ src/Integrate_scRNAseq_GIMVI.py
@@ -1,16 +1,12 @@
 import warnings
 warnings.filterwarnings('ignore')
 
-import cellcharter as cc
-
 import anndata as ad
-import squidpy as sq
 import scanpy as sc
 
 import scvi
 from scvi.external import GIMVI
 from scipy import sparse
-
 
 import numpy as np
 import pandas as pd
@@ -59,8 +55,8 @@ Samples = list(samples_all.obs['dataset'].cat.categories)
 
 
 # Load the single nucleus RNA Seq data
-if args.output is None:
-    filename_sn = os.path.join(FILEPATHBASE, 'data', 'retina_sn_combined.h5ad')
+if args.singlecell is None:
+    filename_sn = os.path.join(FILEPATHBASE, 'data', 'sn_combined.h5ad')
 else:
     filename_sn = args.singlecell
 
@@ -81,42 +77,37 @@ print('Fitting an scVI model to the spatial data...')
 scvi.model.SCVI.setup_anndata(samples_all, layer="counts", batch_key="dataset")
 model = scvi.model.SCVI(samples_all, n_layers=2, n_latent=30, gene_likelihood="nb")
 model.train(early_stopping=True, enable_progress_bar=True)
+print('Done!')
 
+print('Getting latent representation')
 SCVI_LATENT_KEY = "X_scVI"
-samples_all.obsm[SCVI_LATENT_KEY] = model.get_latent_representation().astype(np.float32)
+
+latent = model.get_latent_representation()
+if hasattr(latent, "todense"):
+    latent = latent.todense()
+latent = np.array(latent).astype(np.float32)
+samples_all.obsm[SCVI_LATENT_KEY] = latent
 
 SCVI_NEIGHBORS_KEY = "neighbors_scVI"
 sc.pp.neighbors(samples_all, use_rep=SCVI_LATENT_KEY, key_added=SCVI_NEIGHBORS_KEY)
 sc.tl.leiden(samples_all, key_added="leiden_scVI", neighbors_key=SCVI_NEIGHBORS_KEY)
 
-SCVI_MDE_KEY = "X_scVI_MDE"
-samples_all.obsm[SCVI_MDE_KEY] = scvi.model.utils.mde(samples_all.obsm[SCVI_LATENT_KEY])
+counts_COO = model.posterior_predictive_sample().tocoo()
 
-#sc.pl.embedding(
-#    samples_all,
-#    basis=SCVI_MDE_KEY,
-#    color=["batch", "leiden"],
-#    frameon=False,
-#    ncols=1,
-#)
-samples_all.layers['counts_scvi'] = sparse.csr_matrix(model.posterior_predictive_sample())
+samples_all.layers['counts_scvi'] = sparse.csr_matrix((counts_COO.data, counts_COO.coords), shape=counts_COO.shape)
 print('Done fitting an scVI model to the spatial data. Added samples_all.layers[\'counts_scvi\']')
 
 
 print('Fitting GIMVI model to the single cell and spatial data...')
-# Copy the data for the imputation
-st_adata = samples_all.copy()
-sc_adata = samples_sn.copy()
-
-# filter genes to be the same on the spatial data
-intersect = np.intersect1d(sc_adata.var_names, st_adata.var_names)
-st_adata = st_adata[:, intersect].copy()
-sc_adata = sc_adata[:, intersect].copy()
+# filter genes to be the same on the spatial data and copy the data for the imputation
+intersect = np.intersect1d(samples_sn.var_names, samples_all.var_names)
+st_adata = samples_all[:, intersect].copy()
+sc_adata = samples_sn[:, intersect].copy()
 
 
 # setup_anndata for spatial and sequencing data
 GIMVI.setup_anndata(st_adata, layer="counts", batch_key="dataset")
-GIMVI.setup_anndata(sc_adata, layer="counts", labels_key="CellType")
+GIMVI.setup_anndata(sc_adata, layer="counts", labels_key="CellClass")
 
 model = GIMVI(sc_adata, st_adata)
 model.train(max_epochs=200, early_stopping=True, enable_progress_bar=True)
@@ -128,9 +119,8 @@ def transform(data):
     return np.log(1 + 100 * data)
 
 samples_all[:,st_adata.var_names] = transform(imputed)
-
-
-
+samples_all.layers['GIMVI_imputed'] = samples_all.X
+print('Done!')
 
 # --------------------------------------------------------------------------------
 # Save
